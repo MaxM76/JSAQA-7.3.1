f4e2dcf9183b586113d09b3716591f94e0015a59
MaxM76 <m.v.markelov@mail.ru>
2024-05-23 23:02:33 +0300

    task 1

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -15,3 +15,21 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order if equal books exist", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+        "Гарри Поттер",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
+});
